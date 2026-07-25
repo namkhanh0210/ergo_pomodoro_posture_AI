526fcebc67b28987d9681b0c9dec6dcb741cb80f
namkhanh0210 <namkhanhnguyenquang@gmail.com>
2026-07-26 00:59:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -272,4 +272,4 @@ def analyze_frame(data: ImageData):
         "is_success": is_success
     }
 if __name__ == "__main__":
-  uvicorn.main("app:app", host="0.0.0.0", port=7860, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=7860, reload=True)
