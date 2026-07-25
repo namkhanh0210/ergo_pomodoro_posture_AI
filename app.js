1b4bbb50d2d624591fc3f3fc71983ed00aebcfbb
namkhanh0210 <namkhanhnguyenquang@gmail.com>
2026-07-26 00:43:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -176,7 +176,7 @@ function captureWebcamBase64() {
 
 async function sendImageToAPI(base64Image, isCalibration) {
     try {
-        const response = await fetch("https://ergo-pomodoro-posture-ai.onrender.com/", {
+        const response = await fetch(`${https://ergo-pomodoro-posture-ai.onrender.com/}/api/analyze_frame`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
